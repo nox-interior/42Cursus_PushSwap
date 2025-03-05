2af9e843d1e18d9f375bb4657b8ac2b0339f4b83
Mariam <mariam.arroyo@gmail.com>
2025-03-05 11:42:35 +0000

    Renaming variables for clarity + Norme compliance

--- src/algorithm/08_big_sort_find_target.c
+++ src/algorithm/08_big_sort_find_target.c
@@ -2,7 +2,7 @@
 
 // Finding target nodes
 static void	ft_find_next_highest_index(t_stack_node **stack_a, int index_b,
-									int *target_index, int *target_pos)
+									int *next_hi_idx, int *found_target_pos)
 {
 	t_stack_node	*current_node;
 
@@ -11,44 +11,45 @@ static void	ft_find_next_highest_index(t_stack_node **stack_a, int index_b,
 	current_node = *stack_a;
 	while (current_node != NULL)
 	{
-		if (current_node->index > index_b && current_node->index < *target_index)
+		if (current_node->index > index_b && current_node->index < *next_hi_idx)
 		{
-			*target_index = current_node->index;
-			*target_pos = current_node->position;
+			*next_hi_idx = current_node->index;
+			*found_target_pos = current_node->position;
 		}
 		current_node = current_node->next;
 	}
 }
 
-static void	ft_find_lowest_index(t_stack_node **stack_a, int *target_index,
-									int *target_pos)
+static void	ft_find_lowest_index(t_stack_node **stack_a, int *min_index_a,
+									int *found_target_pos)
 {
-	t_stack_node	*lowest_idx_in_a;
+	t_stack_node	*current_node;
 
-	lowest_idx_in_a = *stack_a;
-	while (lowest_idx_in_a)
+	current_node = *stack_a;
+	while (current_node)
 	{
-		if (lowest_idx_in_a->index < *target_index)
+		if (current_node->index < *min_index_a)
 		{
-			*target_index = lowest_idx_in_a->index;
-			*target_pos = lowest_idx_in_a->position;
+			*min_index_a = current_node->index;
+			*found_target_pos = current_node->position;
 		}
-		lowest_idx_in_a = lowest_idx_in_a->next;
+		current_node = current_node->next;
 	}
 }
 
 int	ft_find_target(t_stack_node **stack_a, int index_b)
 {
-	int				target_index;
-	int				target_pos;
+	int				next_hi_idx;
+	int				found_target_pos;
 
-	target_index = INT_MAX;
-	target_pos = -1;
+	next_hi_idx = INT_MAX;
+	found_target_pos = -1;
 	if (!*stack_a || !stack_a)
 		return (-1);
-	ft_find_next_highest_index(stack_a, index_b, &target_index, &target_pos);
-	if (target_index != INT_MAX)
-		return (target_pos);
-	ft_find_lowest_index(stack_a, &target_index, &target_pos);
-	return (target_pos);
+	ft_find_next_highest_index(stack_a, index_b, &next_hi_idx,
+		&found_target_pos);
+	if (next_hi_idx != INT_MAX)
+		return (found_target_pos);
+	ft_find_lowest_index(stack_a, &next_hi_idx, &found_target_pos);
+	return (found_target_pos);
 }
