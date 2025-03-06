5208421528b18dc5edcec7f6dc0b8cc8ea348057
Mariam <mariam.arroyo@gmail.com>
2025-03-06 16:40:52 +0100

    updated function to solve issue generated by inneficiency when int_max int_min used as arguments.

--- src/algorithm/03_assign_index.c
+++ src/algorithm/03_assign_index.c
@@ -1,51 +1,28 @@
 #include "../../inc/push_swap.h"
 
-static void	ft_find_min_max(t_stack_node **stack_node, int *min, int *max)
-{
-	t_stack_node	*temp_s;
-
-	if (!stack_node)
-		return ;
-	temp_s = *stack_node;
-	*min = temp_s->value;
-	*max = temp_s->value;
-	while (temp_s != NULL)
-	{
-		if (temp_s->value < *min)
-			*min = temp_s->value;
-		if (temp_s->value > *max)
-			*max = temp_s->value;
-		temp_s = temp_s->next;
-	}
-}
-
-void	ft_assign_index(t_stack_node **stack_node)
+void	ft_assign_index(t_stack_node **stack_a)
 {
 	t_stack_node	*current_node;
-	int				index_assigned;
-	int				min;
 	int				max;
-	int				value_to_find;
+	int				len;
 
-	if (!stack_node || !*stack_node)
-		return ;
-	ft_find_min_max(stack_node, &min, &max);
-	index_assigned = 1;
-	value_to_find = min;
-	while (value_to_find <= max)
+	len = ft_stack_len(*stack_a);
+	while (len >= 1)
 	{
-		current_node = *stack_node;
-		while (current_node != NULL)
+		current_node = *stack_a;
+		max = INT_MIN;
+		while (current_node)
 		{
-			if (current_node->value == value_to_find)
-			{
-				current_node->index = index_assigned++;
-				break ;
-			}
+			if (current_node->value > max && current_node->index == -1)
+				max = current_node->value;
 			current_node = current_node->next;
 		}
-		if (value_to_find == INT_MAX)
-			break ;
-		value_to_find++;
+		current_node = *stack_a;
+		while (current_node)
+		{
+			if (current_node->value == max)
+				current_node->index = len--;
+			current_node = current_node->next;
+		}
 	}
 }
